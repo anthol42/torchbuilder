83b67e4f733114bbace915370b210adf2c1ecd43
carcajou666 <alavertu2@gmail.com>
2023-06-15 07:34:20 -0400

    fix(utils_init): patch import error

--- resources/utils_init.py
+++ resources/utils_init.py
@@ -1,4 +1,4 @@
 from .checkpoint import SaveBestModel
 from .feedback import FeedBack, eprint
-from .color import ResetColor, TraceBackColor, Color, ColorPalette
+from .color import ResetColor, TraceBackColor, Color, ColorPalette, RGBColor
 from .resultTable import Table, TableBuilder
